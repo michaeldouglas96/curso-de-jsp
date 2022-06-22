e0e0aca03b7e9da10cd29c0ebd519bd70a49e8cd
van_r <van_r@LAPTOP-KJVQDBRR>
2022-06-22 02:30:07 -0300

    projeto em andamento

--- curso-de-jsp/src/main/java/model/ModelLogin.java
+++ curso-de-jsp/src/main/java/model/ModelLogin.java
@@ -8,6 +8,30 @@ public class ModelLogin implements Serializable {
 
 	private String login;
 	private String senha;
+	private String nome;
+	private String email;
+	private Long id;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getLogin() {
 		return login;
 	}
